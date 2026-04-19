3aa5dfcb70900620bbd9446cacf8396c8e47734a
Jiga <manjumusei@gmail.com>
2026-04-19 13:41:41 +0800

    remove unused CLI and main entry point files

--- __init__.py
+++
@@ -1,3 +0,0 @@
-#EntropyGarden
-"""EntropyGarden - My project where you can grow your own crytpgraphic key hiearchy from image entrophy densities"""
-__version__ = "1.0.0"
--- main.py
+++
@@ -1,7 +0,0 @@
-"""Main entry point for the project"""
-from entrophygarden.cli import main
-
-
-if __name__ == "__main+__":
-    main()
-    
